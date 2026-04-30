733daef7b2ce49710e1317b5799b57b9692f6bdd
ounishedyl <ounishedyl@gmail.com>
2026-04-30 19:40:39 +0100

    Week 2 lab completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
+const courseTitle = "React Fundamentals";
+
 function App() {
+  const studentName = "Hedyl";
+
+  const student = {
+    name: "Hedyl",
+    age: 20,
+    track: "Web Development"
+  };
+
+  function sayHello() {
+    return `Hello ${studentName}, welcome to React!`;
+  }
+
   return (
-    <>
-      <h1>Hello My React Project 🚀</h1>
-      <p>This is my Lab 1 React assignment</p>
-    </>
-  )
+    <div>
+      <h1>My First React Component </h1>
+
+      <p>Student name: {studentName}</p>
+      <p>Course: {courseTitle}</p>
+
+      <h2>Welcome to {courseTitle}, {studentName}!</h2>
+
+      <label htmlFor="studentInput">Enter your name:</label>
+      <input type="text" id="studentInput" />
+
+      <p>Name: {student.name}</p>
+      <p>Age: {student.age}</p>
+      <p>Track: {student.track}</p>
+
+      <p>{sayHello()}</p>
+    </div>
+  );
 }
 
-export default App
+export default App;
